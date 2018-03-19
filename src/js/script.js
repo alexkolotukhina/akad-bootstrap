ebccdc57cd41de2e6737d86591345629e504a7a2
alexkolotukhina <alex_kolotukhina@outlook.com>
2018-03-19 18:31:40 +0200

    first-commit

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+//=require jquery.min.js
+
+// to inlude plugins add "=" sign below
+//=require bootstrap.min.js
+// require slick.min.js 
+
+;(function($){
+
+	$(document).ready(function(){
+		$('.carousel').carousel({
+		  interval: 2000
+		})
+	});
+
+})(jQuery);
